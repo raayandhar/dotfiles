c43b1d15ab108384a435daf583b3e8b14cf2c7fb
raayandhar <raayan.dhar@gmail.com>
2024-07-18 22:32:21 -0700

    Update -- currently broken: Wrong number of arguments: setq, 3

--- .emacs.el
+++ .emacs.el
@@ -24,13 +24,13 @@
 (setq inhibit-startup-screen t)
 
 ;; Disable the toolbar
-(tool-bar-mode -1)
+(setq tool-bar-mode -1)
 
 ;; Disable the menu bar
-(menu-bar-mode -1)
+(setq menu-bar-mode -1)
 
 ;; Disable visible scrollbar
-(scroll-bar-mode -1)
+(setq scroll-bar-mode -1)
 
 ;; Simplify the 'yes' or 'no' to 'y' or 'n'
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -169,7 +169,7 @@
 
 ;; Show the matching parenthesis and do it immediately, we are in a hurry
 (setq show-paren-delay 0)
-(show-paren-mode t)
+(setq show-paren-mode t)
 
 ;; use colorization for all modes
 (global-font-lock-mode t)
@@ -229,3 +229,270 @@ load-warning buffer in case of failure."
       (fit-window-to-buffer (get-buffer-window buf))
       (set-buffer-modified-p nil))
     nil))
+
+;; Hold up to 1000 in *Messages* buffer
+(setq message-log-max 1000)
+
+;; Only logical line jumping
+(setq line-move-visual nil)
+
+;; Do not keep track of autosaved files
+(setq auto-save-list-file-prefix nil)
+
+;; Show empty lines at end of buffer
+(setq default-indicate-empty lines t)
+
+;; Show me the region until I do something on it
+(setq transient-mark-mode t)
+
+;; Do not color stuff which is clickable when hovering over it
+(setq mouse-highlight nil)
+
+;; SSH !!!
+(setq tramp-default-method "ssh")
+
+;; Files deserves local vars
+(setq enable-local-eval t)
+
+(setq-default
+
+ ;; Show white spaces at the end of lines
+ show-trailing-whitespace t
+
+ ;; Do not show the cursor in non-active window
+ cursor-in-non-selected-windows nil
+
+ use-dialog-box nil
+ use-file-dialog nil
+
+ ;; when on a TAB, the cursor has the TAB length
+ x-stretch-cursor t
+
+ ;; This is the default coding system when toggle-input-method is
+ ;; invoked (C-\)
+ default-input-method "latin-1-prefix"
+
+ ;; do not put tabs when indenting
+ indent-tabs-mode nil
+ ;; Stop indenting automatically, that's annoying
+ electric-indent-chars nil
+
+ ;; And yes, we have a fast display / connection / whatever
+ baud-rate 524288
+ ;; baud-rate 10
+
+ ;; To keep the cursor always visible when it moves (thanks
+ ;; snogglethrop!)
+ redisplay-dont-pause t
+
+ ;; I want to see the keys I type instantaneously
+ echo-keystrokes 0.1
+ )
+
+;; Show the column number
+(column-number-mode 1)
+
+;; What modes for what file extentions
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(require 'org-table)
+
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . (lambda()
+                                                (text-mode)
+                                                (orgtbl-mode)
+                                                ;; (auto-fill-mode)
+                                                (flyspell-mode))))
+
+(add-hook 'c++-mode-hook 'flyspell-prog-mode)
+;; (add-hook 'lua-mode-hook 'flyspell-prog-mode)
+(add-hook 'log-edit-mode-hook 'flyspell-mode)
+
+(add-hook 'markdown-mode-hook 'flyspell-mode)
+(add-hook 'markdown-mode-hook 'auto-fill-mode)
+
+;; I am a power-user
+
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Appearance
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(when (boundp 'x-display-name)
+
+  (setq-default
+
+   ;; If the display is :0.0, we make the assumption that we are
+   ;; running the emacs locally, and we do not show the
+   ;; hostname. Otherwise, show @host.
+
+   frame-title-format (concat "emacs" ;;invocation-name
+                              (unless (string= x-display-name ":0.0")
+                                (concat "@" system-name))
+                              " (%b)")
+
+   ;; Use the same for the icone
+
+   icon-title-format frame-title-format
+   ))
+
+;; "tool" bar? Are you kidding?
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
+;; My funky setting of face colors. Basically, we switch to a sober
+;; look and darken a bit the colors which need to (because of the
+;; darker background)
+
+(defun ff/configure-faces (fl)
+  "Set face attributes and create faces when necessary"
+  (mapc (lambda (f)
+          (unless (boundp (car f)) (make-empty-face (car f)))
+          (eval `(set-face-attribute (car f) nil ,@(cdr f))))
+        fl))
+
+;; Not the same in xterm (which is gray in my case) and in
+;; X-window
+
+(when window-system
+
+  (ff/configure-faces
+   '(
+     ;; (escape-glyph :foreground "#c0c0c0" :weight 'bold)
+     (escape-glyph :foreground "green3" :weight 'bold)
+     (default :background "gray90" :foreground "black")
+     (cperl-array-face :background "gray90" :foreground "blue" :weight 'bold)
+     (cperl-hash-face :background "gray90" :foreground "purple" :weight 'bold)
+     (message-cited-text :foreground "red4")
+     (diff-mode :background "gray90" :weight 'bold)
+     (diff-added :background "gray90" :foreground "green4" :weight 'bold)
+     (diff-removed :background "gray90" :foreground "red2" :weight 'bold)
+     (diff-changed :background "gray90" :foreground "blue" :weight 'bold)
+     (diff-file-header :background "white" :foreground "black"
+                       :weight 'bold)
+     (diff-header :background "white" :foreground "black")
+     (diff-hunk-header :background "white" :foreground "black")
+     (font-lock-builtin-face :foreground "deeppink3")
+     (font-lock-string-face :foreground "dark olive green")
+     (font-lock-variable-name-face :foreground "sienna")
+     ;; (font-lock-function-name-face :foreground "blue" :weight 'bold)
+     (font-lock-function-name-face :foreground "blue")
+     ;; (font-lock-comment-delimiter-face :foreground "dark violet")
+     ;; (font-lock-comment-face :foreground "dark violet")
+     (flyspell-incorrect :background "#ff0000" :foreground "black")
+     (flyspell-duplicate :background "#ff9000" :foreground "black")
+     (hl-line :background "white")
+     (sh-heredoc :foreground "black" :background "#fff0f0")
+     (sh-heredoc-face :foreground "black" :background "#fff0f0")
+     (header-line :background "gray65")
+     (highlight :background "white")
+     (message-cited-text-face :foreground "firebrick")
+     (isearch :background "yellow" :foreground "black")
+     (isearch-lazy-highlight-face' :background "yellow3" :foreground "black")
+     (region :background "#b8b8e0" :foreground "black")
+     ;; (region :background "plum" :foreground "black")
+     (show-paren-match-face :background "gold" :foreground "black")
+     (show-paren-mismatch-face :background "red" :foreground "black")
+     (trailing-whitespace :background "gray65")
+     (cursor :inverse-video t)
+     (enotes/list-title-face :foreground "blue" :weight 'bold)
+     (mode-line :background "#b0b0ff" :foreground "black" :box nil
+                :inverse-video nil)
+     (header-line :background "cornflowerblue" :foreground "black" :box nil
+                  :inverse-video nil)
+     (mode-line-inactive :background "gray80" :foreground "black" :box nil
+                         :inverse-video nil)
+     ;; (fringe :background "black" :foreground "gray90")
+     (fringe :background "gray80")
+     (ff/date-info-face :foreground "white")
+     (ff/battery-info-face :foreground "blue")
+     (ff/battery-info-alarm-face :foreground "red")
+     ;; (ff/mail-alarm-face :foreground "white" :background "red2")
+     ;; (alarm-vc-face :foreground "black" :background "yellow" :weight 'normal)
+     (gui-button-face :background "green" :foreground "black")
+     ))
+  )
+
+(unless window-system
+  ;; (xterm-mouse-mode 1)
+  (ff/configure-faces
+   '((italic :underline nil)
+     (info-title-2 :foreground "green")
+     (font-lock-comment-delimiter-face :foreground "green")
+     (font-lock-comment-face :foreground "green")
+     (cperl-array-face :background "gray90" :foreground "blue" :weight 'bold)
+     (cperl-hash-face :background "gray90" :foreground "purple" :weight 'bold)
+     (diff-added :background "gray90" :foreground "green4" :weight 'bold)
+     (diff-removed :background "gray90" :foreground "red2" :weight 'bold)
+     (diff-changed :background "gray90" :foreground "blue" :weight 'bold)
+     (diff-file-header-face :background "white" :foreground "black"
+                            :weight 'bold)
+     (diff-header-face :background "white" :foreground "black")
+     (diff-hunk-header-face :background "white" :foreground "black")
+     (diff-indicator-removed :foreground "red" :weight 'bold)
+     (diff-removed :foreground "red" :weight 'bold)
+     (diff-indicator-added :foreground "blue" :weight 'bold)
+     (diff-added :foreground "blue" :weight 'bold)
+     (font-lock-string-face :foreground "green")
+     (font-lock-variable-name-face :foreground "blue")
+     (font-lock-constant-face :foreground "blue")
+     (font-lock-preprocessor-face :foreground "green")
+     (font-lock-function-name-face :foreground "cyan")
+     (flyspell-incorrect :foreground "red2")
+     (flyspell-duplicate :foreground "OrangeRed2")
+     (hl-line :background "white")
+     (sh-heredoc :foreground "black" :background "#fff0f0")
+     (sh-heredoc-face :foreground "black" :background "#fff0f0")
+     (font-lock-keyword-face :foreground "blue")
+     (highlight :background "white")
+     (isearch :background "orange" :foreground "black")
+     (isearch-lazy-highlight-face' :background "yellow" :foreground "black")
+     ;; (display-time-mail-face :background "white")
+     (show-paren-match-face :background "gold" :foreground "black")
+     (show-paren-mismatch-face :background "red" :foreground "black")
+     (trailing-whitespace :background "white")
+     (mode-line :background "cornflowerblue" :foreground "black" :box nil
+                :inverse-video nil)
+     (header-line :background "cornflowerblue" :foreground "black" :box nil
+                  :inverse-video nil)
+     (mode-line-inactive :background "gray60" :foreground "black" :box nil
+                         :inverse-video nil)
+     (region :background "white" :foreground "black")
+     (ff/date-info-face :foreground "white" :weight 'bold)
+     (ff/battery-info-face :foreground "black")
+     (ff/battery-info-alarm-face :foreground "red")
+     ;; (ff/mail-alarm-face :foreground "red" :weight 'bold)
+     (selector/selection :background "yellow")
+     (gui-button-face :background "green" :foreground "white")
+     (enotes/information-face :foreground "cyan")
+
+     (file-name-shadow :foreground "black")
+     (shadow :foreground "black")
+     (warning :foreground "black" :background "red")
+     ))
+  )
+
+;; When we are root, put the modeline in red
+
+(when (string= (user-real-login-name) "root")
+  (ff/configure-faces
+   '((mode-line :background "red3" :foreground "black" :box nil
+                :inverse-video nil))
+   ))
+
+;; Why should I have to do this?
+(add-hook 'sh-mode-hook
+          (lambda ()
+            (set-face-attribute 'sh-heredoc nil
+                                :foreground "#604000"
+                                :background "white"
+                                :italic t)
+            (set-face-attribute 'sh-heredoc-face nil
+                                :foreground "#604000"
+                                :background "white"
+                                :italic t)
+            ))
+

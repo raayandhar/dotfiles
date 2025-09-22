a8ac8e9c97bc0f0842c0aae4324911e4f233e5a7
raayandhar <raayan.dhar@gmail.com>
2025-09-21 22:11:44 -0700

    update dotfiles

---
+++ emacs/early-init.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- Early init tweaks -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Disable package.el since we use straight.el
+
+;;; Code:
+
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
